f7731f0e43de155f513e52601c89d3d04a3eac09
Zane Grasso <ztg6893@rit.edu>
2015-04-14 12:43:19 -0400

    testing scripts for smoothers started

--- tests/misc/smoothers_test.js
+++ tests/misc/smoothers_test.js
@@ -1,5 +1,21 @@
 module('smoothers');
 
 test('mg_pow_weight', function() {
-
+	equal(_pow_weight(2,0),0,'');
+	equal(_pow_weight(1,2),0,'');
+	equal(_pow_weight(0,1),1,'');
+	equal(_pow_weight(0.5,3),0.669921875,'');
+	equal(_pow_weight(0,1),2,'');
 });
+
+
+/*test('mq_bisquare_weight', function(){
+
+
+})
+
+test('mq_tricube_weight', function(){
+
+
+})
+*/
