df32214f17549555aeb1d2e09627bf6df88c6d2f
Zach Berger <zach@zberger.com>
2015-04-14 12:42:26 -0400

    Corrected incorrect spelling of equal

--- tests/misc/smoothers_test.js
+++ tests/misc/smoothers_test.js
@@ -11,20 +11,24 @@ test('mg_neighborhood_width', function(){
 	var some_ordered = [ 3, 7, 11 ];
 	var some_unordered = [ 11, 3, 7 ];
 
-	equals( _neighborhood_width(empty, 0), null );
-	equals( _neighborhood_width(one, 5), 2 );
-	equals( _neighborhood_width(some_ordered, 5), 6);
-	equals( _neighborhood_width(some_unordered, 5), 6);
+	equal( _neighborhood_width(empty, 0), null );
+	equal( _neighborhood_width(one, 5), 2 );
+	equal( _neighborhood_width(some_ordered, 5), 6);
+	equal( _neighborhood_width(some_unordered, 5), 6);
 });
 
 test('mg_manhattan', function(){
 
-	equals(_manhattan(0,0), 0);
-	equals(_manhattan(1,0), 1);
-	equals(_manhattan(0,1), 1);
-	equals(_manhattan(0,5), 5);
-	equals(_manhattan(5,0), 5);
-	equals(_manhtttan(1,5), 4);
-	equals(_manhattan(5,1), 4);
+	equal(_manhattan(0,0), 7);
+	equal(_manhattan(1,0), 1);
+	equal(_manhattan(0,1), 1);
+	equal(_manhattan(0,5), 5);
+	equal(_manhattan(5,0), 5);
+	equal(_manhtttan(1,5), 4);
+	equal(_manhattan(5,1), 4);
+
+});
+
+test('mg_weighted_means', function(){
 
 });
