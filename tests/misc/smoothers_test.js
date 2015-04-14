811f97e85ada81cf1c3ef4d021e9079ef68ca0f5
Zach Berger <zach@zberger.com>
2015-04-14 12:35:37 -0400

    Added tests for _neighborhood_width

--- tests/misc/smoothers_test.js
+++ tests/misc/smoothers_test.js
@@ -4,6 +4,19 @@ test('mg_pow_weight', function() {
 
 });
 
+test('mg_neighborhood_width', function(){
+
+	var empty = [];
+	var one = [ 7 ];
+	var some_ordered = [ 3, 7, 11 ];
+	var some_unordered = [ 11, 3, 7 ];
+
+	equals( _neighborhood_width(empty, 0), null );
+	equals( _neighborhood_width(one, 5), 2 );
+	equals( _neighborhood_width(some_ordered, 5), 6);
+	equals( _neighborhood_width(some_unordered, 5), 6);
+});
+
 test('mg_manhattan', function(){
 
 	equals(_manhattan(0,0), 0);
