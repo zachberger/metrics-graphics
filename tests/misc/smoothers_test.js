e320215f64e57aa052a66f76d9cdb82af8f43e91
Zach Berger <zach@zberger.com>
2015-04-14 12:30:13 -0400

    Added tests for _manhattan

--- tests/misc/smoothers_test.js
+++ tests/misc/smoothers_test.js
@@ -3,3 +3,15 @@ module('smoothers');
 test('mg_pow_weight', function() {
 
 });
+
+test('mg_manhattan', function(){
+
+	equals(_manhattan(0,0), 0);
+	equals(_manhattan(1,0), 1);
+	equals(_manhattan(0,1), 1);
+	equals(_manhattan(0,5), 5);
+	equals(_manhattan(5,0), 5);
+	equals(_manhtttan(1,5), 4);
+	equals(_manhattan(5,1), 4);
+
+});
